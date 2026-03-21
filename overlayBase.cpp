eb6e4ee2a1705d51b9dfd7feacc73fd638f9fcdd
G3ng4rw1z4rd <245198848+G3ng4rw1z4rd@users.noreply.github.com>
2026-03-21 08:40:05 -0300

    taskbar support

--- overlayBase.cpp
+++ overlayBase.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 #include "Stats.h"
 
-
 int main() {
-	monitor m;
-	m.readerCli();
+    monitor m;
+
+    std::thread t1(&monitor::readerCli, &m);
+    std::thread t2(&monitor::drawTaskbar, &m);
+
+    t1.join();
+    t2.join();
 }
  
 
